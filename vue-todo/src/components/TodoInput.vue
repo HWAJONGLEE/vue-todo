4b1d9c247304756f50c09fb8c2d899dda300fc2c
HWAJONGLEE <wowlover10@nate.com>
2021-02-02 16:51:22 +0900

    vue-todo 앱 start

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <input type="text" v-model="newTodoItem">
+  </div>
+</template>
+
+<script>
+export default {
+    data (){
+        return{
+            newTodoItem: "",
+        }
+    },
+    methods : {
+        addTodo (){
+            console.log(this.newTodoItem);
+            this.newTodoItem='';
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
